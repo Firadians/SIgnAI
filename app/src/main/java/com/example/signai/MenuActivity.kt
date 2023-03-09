b64d6a6bd578df8d24d046b7ab92108ed1a1face
Muhammad Firdaus A <mfirdausa30@gmail.com>
2023-03-09 19:08:31 +0700

    initial commit

--- app/src/main/java/com/example/signai/MenuActivity.kt
+++ app/src/main/java/com/example/signai/MenuActivity.kt
@@ -7,5 +7,11 @@ class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        supportActionBar?.hide()
+        val fragOne = HomeFragment()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.main_container, fragOne)
+            commit()
+        }
     }
 }
